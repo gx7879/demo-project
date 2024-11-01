1aedef8e909eecd17934d745ef90dd2f523024be
吳啟佑 <wilsonwu@double-cash.com>
2024-11-01 11:02:14 +0800

    resetPassword page complete

--- pages/resetPassword.vue
+++ pages/resetPassword.vue
@@ -1,5 +1,77 @@
-<script setup></script>
+<script setup>
+function onSubmit(values) {
+  console.log("submit", values);
+}
+</script>
 
-<template></template>
+<template>
+  <div class="max-w-[720px] mx-auto py-[100px]">
+    <div class="max-w-[460px] mx-auto">
+      <h1 class="text-[28px] leading-10 text-main-black/80 font-bold mb-12">
+        設定新的密碼
+      </h1>
+      <VeeForm class="max-w-[460px] mx-auto mb-[60px]" @submit="onSubmit">
+        <div class="mb-5 text-left">
+          <label
+            for="email"
+            class="block mb-2 text-xl font-light text-main-black text-left"
+            >設定新密碼
+          </label>
+          <VeeField
+            type="password"
+            name="password"
+            label="密碼"
+            class="bg-gray-50 border border-[#b1b1b1] placeholder:text-[#b3b3b3] text-lg rounded-[10px] block w-full p-2.5"
+            placeholder="6~20個字且须包含至少一個英文字母和一個數字"
+            :rules="{
+              required: true,
+              min: 6,
+              max: 20,
+              regex: /^(?=.*[a-zA-Z])(?=.*\d).+$/,
+            }"
+          />
+          <VeeErrorMessage name="password" class="text-error-msg text-sm" />
+        </div>
+        <div class="mb-5 text-left">
+          <label
+            for="password"
+            class="block mb-2 text-xl font-light text-main-black text-left"
+            >確認新密碼
+          </label>
+          <VeeField
+            type="password"
+            name="confirmPassword"
+            label="密碼"
+            class="bg-gray-50 border border-[#b1b1b1] placeholder:text-[#b3b3b3] text-lg rounded-[10px] block w-full p-2.5"
+            placeholder="再次輸入新密碼"
+            rules="required|confirmed:@password|regex: /^(?=.*[a-zA-Z])(?=.*\d).+$/"
+          />
+          <VeeErrorMessage
+            name="confirmPassword"
+            class="text-error-msg text-sm"
+          />
+        </div>
+        <button
+          type="submit"
+          class="text-white bg-[#4c4341] font-bold rounded-[5px] text-lg w-full p-3 mb-6"
+        >
+          更改密碼
+        </button>
+      </VeeForm>
+    </div>
+    <div class="border-main-black border-b my-[60px]"></div>
+    <div class="flex flex-col items-center">
+      <NuxtLink
+        to="/login"
+        class="text-[#4b4240] text-lg font-normal underline mb-6"
+      >
+        會員登入
+      </NuxtLink>
+      <NuxtLink to="/" class="text-[#4b4240] text-lg font-normal underline">
+        新用戶註冊
+      </NuxtLink>
+    </div>
+  </div>
+</template>
 
 <style></style>
