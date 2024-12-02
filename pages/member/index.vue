d64e87dbe541ef25a673c0452bf7b65581e82fab
吳啟佑 <wilsonwu@double-cash.com>
2024-12-02 18:53:13 +0800

    member info edit complete

--- pages/member/index.vue
+++ pages/member/index.vue
@@ -5,6 +5,14 @@ const dateFormat = "YYYY/MM/DD";
 
 const now = dayjs();
 const value1 = ref(null);
+const info = ref({
+  lastName: null,
+  firstName: null,
+  birthday: null,
+  sex: null,
+  phone: null,
+  address: null,
+});
 const store = useResetPasswordStore();
 const { showResetPasswordModal } = store;
 const { resetPasswordAuth } = storeToRefs(store);
@@ -28,6 +36,12 @@ function editInfo() {
 const isFbLogin = ref(true);
 const isGoogleLogin = ref(false);
 const isAppleLogin = ref(false);
+
+function onSubmit(values) {
+  console.log("submit", values);
+  info.value = values;
+  editInfoStatus.value = false;
+}
 </script>
 
 <template>
@@ -94,6 +108,7 @@ const isAppleLogin = ref(false);
     >
       基本資料
       <button
+        v-if="!(editInfoStatus && resetPasswordAuth)"
         @click="editInfo"
         class="w-[98px] text-lg leading-none font-bold px-3 py-[9px] rounded-[5px] bg-main-black/80 text-white"
       >
@@ -101,7 +116,7 @@ const isAppleLogin = ref(false);
       </button>
     </h2>
     <template v-if="editInfoStatus && resetPasswordAuth">
-      <div class="space-y-6 max-w-[660px]">
+      <VeeForm class="space-y-6 max-w-[660px" @submit="onSubmit">
         <div class="grid sm:grid-cols-[1fr,_2fr] sm:gap-x-3">
           <div>
             <label
@@ -139,30 +154,150 @@ const isAppleLogin = ref(false);
         <div class="grid sm:grid-cols-[1fr,_2fr] sm:gap-x-3">
           <div>
             <label
-              for="recipientPhone"
+              for="birthday"
               class="block text-xl text-main-black/70 font-normal mb-3"
             >
               生日
             </label>
             <!-- eslint-disable vue/no-v-model-argument -->
-            <DatePicker
+            <VeeField
               v-model:value="value1"
-              placeholder="年/月/日"
-              popupClassName="datepicker-modal"
-            ></DatePicker>
+              type="text"
+              name="birthday"
+              v-slot="{ field }"
+              rules="required"
+            >
+              <DatePicker
+                id="birthday"
+                v-bind="field"
+                placeholder="年/月/日"
+                popupClassName="datepicker-modal"
+                :valueFormat="dateFormat"
+              ></DatePicker>
+            </VeeField>
+            <VeeErrorMessage name="birthday" class="text-error-msg text-sm" />
           </div>
         </div>
-      </div>
+        <div>
+          <label
+            for="Sex"
+            class="block text-xl text-main-black/70 font-normal mb-3"
+            >性別
+          </label>
+
+          <ul
+            class="grid w-full gap-x-3 sm:grid-cols-3 text-lg leading-none font-bold text-main-black/80"
+          >
+            <li>
+              <VeeField
+                id="female"
+                name="sex"
+                type="radio"
+                value="female"
+                class="hidden peer"
+                rules="required"
+              />
+              <label
+                for="female"
+                class="inline-flex items-center justify-center w-full p-4 bg-white border border-main-black/80 rounded-lg cursor-pointer peer-checked:text-white peer-checked:bg-main-black/80 hover:text-gray-600 hover:bg-gray-100"
+              >
+                女
+              </label>
+            </li>
+            <li>
+              <VeeField
+                type="radio"
+                id="male"
+                name="sex"
+                value="male"
+                class="hidden peer"
+                rules="required"
+              />
+              <label
+                for="male"
+                class="inline-flex items-center justify-center w-full p-4 bg-white border border-main-black/80 rounded-lg cursor-pointer peer-checked:text-white peer-checked:bg-main-black/80 hover:text-gray-600 hover:bg-gray-100"
+              >
+                男
+              </label>
+            </li>
+            <li>
+              <VeeField
+                type="radio"
+                id="other"
+                name="sex"
+                value="other"
+                class="hidden peer"
+                rules="required"
+              />
+              <label
+                for="other"
+                class="inline-flex items-center justify-center w-full p-4 bg-white border border-main-black/80 rounded-lg cursor-pointer peer-checked:text-white peer-checked:bg-main-black/80 hover:text-gray-600 hover:bg-gray-100"
+              >
+                其他
+              </label>
+            </li>
+          </ul>
+          <VeeErrorMessage name="sex" class="text-error-msg text-sm" />
+        </div>
+        <div>
+          <label
+            for="phone"
+            class="block text-xl text-main-black/70 font-normal mb-3"
+          >
+            連絡電話
+          </label>
+          <VeeField
+            type="text"
+            name="phone"
+            class="bg-gray-50 border border-notice-gray placeholder:text-[#b3b3b3] text-lg leading-[26px] rounded-[10px] block w-full px-4 py-[15px]"
+            placeholder="請輸入連絡電話"
+            rules="required"
+          />
+          <VeeErrorMessage name="phone" class="text-error-msg text-sm" />
+        </div>
+        <div>
+          <label
+            for="address"
+            class="block text-xl text-main-black/70 font-normal mb-3"
+          >
+            地址
+          </label>
+          <VeeField
+            type="text"
+            name="address"
+            class="bg-gray-50 border border-notice-gray placeholder:text-[#b3b3b3] text-lg leading-[26px] rounded-[10px] block w-full px-4 py-[15px]"
+            placeholder="台北市中正區中正路123號"
+            rules="required"
+          />
+          <VeeErrorMessage name="address" class="text-error-msg text-sm" />
+        </div>
+        <button
+          class="w-full bg-main-black/80 rounded-[5px] h-[52px] text-white text-lg leading-none font-bold"
+        >
+          儲存
+        </button>
+      </VeeForm>
     </template>
     <template v-else>
       <div class="pt-6 space-y-6 border-t border-main-black">
-        <div class="text-xl font-medium text-main-black/70">姓氏 : 未填寫</div>
-        <div class="text-xl font-medium text-main-black/70">名字 : 未填寫</div>
-        <div class="text-xl font-medium text-main-black/70">性別 : 未填寫</div>
         <div class="text-xl font-medium text-main-black/70">
-          連絡電話 : 未填寫
+          姓氏 : {{ info?.firstName ?? "未填寫" }}
         </div>
-        <div class="text-xl font-medium text-main-black/70">地址 : 未填寫</div>
+        <div class="text-xl font-medium text-main-black/70">
+          名字 : {{ info?.lastName ?? "未填寫" }}
+        </div>
+        <div class="text-xl font-medium text-main-black/70">
+          生日 : {{ info?.birthday ?? "未填寫" }}
+        </div>
+        <div class="text-xl font-medium text-main-black/70">
+          性別 : {{ info?.sex ?? "未填寫" }}
+        </div>
+        <div class="text-xl font-medium text-main-black/70">
+          連絡電話 : {{ info?.phone ?? "未填寫" }}
+        </div>
+        <div class="text-xl font-medium text-main-black/70">
+          地址 : {{ info?.address ?? "未填寫" }}
+        </div>
       </div>
     </template>
   </div>
